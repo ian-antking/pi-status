8797b172fe57476a51168bf184216902039f59cc
Ian King <ian.antking@gmail.com>
2020-04-26 15:56:04 +0100

    add simple app

---
+++ pi_status/app.py
@@ -0,0 +1,30 @@
+
+class App():
+  def __init__(self, blinkt_manager):
+    self.blinkt = blinkt_manager
+    self.blinkt.set_brightness(0.1)
+    self.clear()
+
+  def clear(self):
+    self.blinkt.clear()
+
+  def busy(self):
+    self.clear()
+    for i in range(8):
+      self.blinkt.set_pixel(i, 255, 0, 0)
+    
+
+
+if __name__ == '__main__':
+  import blinkt
+  import buttonshim
+  import signal
+
+  app = App(blinkt)
+
+  @buttonshim.on_press(buttonshim.BUTTON_A)
+  def button_a(button, presses):
+    app.busy()
+
+  signal.pause()
+
