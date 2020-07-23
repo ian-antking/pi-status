02bd1ba43556b22b8e41902fd67ec1d01c259c44
ian-antking <ian.antking@gmail.com>
2020-07-23 12:50:34 +0100

    remove blinkt and buttonshim from app

--- pi_status/app.py
+++ pi_status/app.py
@@ -1,59 +1,8 @@
 
 class App():
-  def __init__(self, blinkt_manager):
-    self.blinkt = blinkt_manager
-    self.on = True
-    self.status = 'idle'
-
-    self.blinkt.set_brightness(0.1)
-    self.clear()
-
-  def toggle_on(self):
-    print('Setting self.on to %s' % self.on)
-    self.on = not self.on
-
-  def clear(self):
-    self.blinkt.set_all(0, 0, 0)
-
-
-  def busy(self):
-    self.blinkt.set_all(255, 0, 0)
-
-  
-  def working(self):
-    self.blinkt.set_all(255, 191, 0)
-
-
-  def available(self):
-    self.blinkt.set_all(0, 255, 0)
-
-
-  def update_leds(self):
-    self.blinkt.show()
-    
+  def __init__(self):
+    print('pi-status started')   
 
 if __name__ == '__main__':
-  import blinkt
-  import buttonshim
-
-  app = App(blinkt)
-
-  @buttonshim.on_press(buttonshim.BUTTON_A)
-  def button_a(button, presses):
-    app.clear()
-
-  @buttonshim.on_press(buttonshim.BUTTON_B)
-  def button_b(button, presses):
-    app.busy()
-
-  @buttonshim.on_press(buttonshim.BUTTON_C)
-  def button_c(button, presses):
-    app.working()
-
-  @buttonshim.on_press(buttonshim.BUTTON_D)
-  def button_d(button, presses):
-    app.available()
-
-  while True:
-    app.update_leds()
+  print('running...')
 
