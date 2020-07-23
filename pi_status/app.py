d531105d8b7b761d38a7fea4765a45929b64be3c
ian-antking <ian.antking@gmail.com>
2020-07-23 18:17:53 +0100

    don't reassign color if not included in message

--- pi_status/app.py
+++ pi_status/app.py
@@ -3,14 +3,14 @@ import json
 class App:
   def __init__(self, led_manager):
     self.color = (168, 168, 168)
+    self.mode = "solid"
     self.led_manager = led_manager
     self.led_manager.update_light(self.color)
   
   def on_message(self, client, user_date, message):
     payload = str(message.payload.decode("utf-8"))
     data = json.loads(payload)
-    color = eval(data["color"])
-    self.color = color
+    self.color = eval(data["color"]) or self.color
 
   def update(self):
-    self.led_manager.update_light(self.color)
+    self.led_manager.update_light(self.color, self.mode)
