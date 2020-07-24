de7a60eb3ab13ea46d284b609997ef884ff57f5d
ian-antking <ian.antking@gmail.com>
2020-07-24 09:33:53 +0100

    change getopts to argparse

--- __main__.py
+++ __main__.py
@@ -2,35 +2,20 @@ import paho.mqtt.client as mqtt
 import signal
 import sys
 from pi_status import App
+from pi_status import UnicornManager
 from dotenv import load_dotenv
-from getopt import getopt
+from argparse import ArgumentParser
 import os
 
 load_dotenv()
 
-argv = sys.argv[1:]
-opts, args = getopt(argv, "h:", ["hat"])
+parser = ArgumentParser()
+parser.add_argument("--hat", "-h", required=True, choices=["unicorn-phat"], help="type of hat")
+arge = parser.parse_args()
 
-hat = None
+print(args.hat)
 
-try:
-  for opt, arg in opts:
-      if opt in ("-h", "--hat"):
-        hat = arg
-
-  if not hat:
-    raise Exception("--hat option required") 
-
-  if hat == "unicorn":
-    from pi_status import UnicornManager
-    led_manager = UnicornManager()
-  else:
-    raise Exception("--hat option must be unicorn") 
-except Exception as e:
-    print(e)
-    sys.exit()
-
-app = App(led_manager)
+app = App(UnicornManager())
 
 client = mqtt.Client("DEVICE_NAME")
 client.on_message = app.on_message
