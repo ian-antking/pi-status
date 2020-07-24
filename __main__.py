70c25427213a0adea26c8e6ec4d718f6d7f36277
ian-antking <ian.antking@gmail.com>
2020-07-24 09:21:49 +0100

    use hat option to import correct manager

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ import paho.mqtt.client as mqtt
 import signal
 import sys
 from pi_status import App
-from pi_status import UnicornManager
 from dotenv import load_dotenv
 from getopt import getopt
 import os
@@ -14,16 +13,24 @@ opts, args = getopt(argv, "h:", ["hat"])
 
 hat = None
 
-for opt, arg in opts:
-    if opt in ("-h", "--hat"):
-      hat = arg
+try:
+  for opt, arg in opts:
+      if opt in ("-h", "--hat"):
+        hat = arg
 
-if not hat:
-  raise Exception("--hat option required") 
+  if not hat:
+    raise Exception("--hat option required") 
 
-print(hat)
+  if hat == "unicorn":
+    from pi_status import UnicornManager
+    led_manager = UnicornManager()
+  else:
+    raise Exception("--hat option must be unicorn") 
+except Exception as e:
+    print(e)
+    sys.exit()
 
-app = App(UnicornManager())
+app = App(led_manager)
 
 client = mqtt.Client("DEVICE_NAME")
 client.on_message = app.on_message
