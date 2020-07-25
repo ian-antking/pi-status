8ea56063fe7ae0127cff14c7ef22b42e51d3b8e5
ian-antking <ian.antking@gmail.com>
2020-07-25 23:10:33 +0100

    add off mode

--- pi_status/unicorn_manager.py
+++ pi_status/unicorn_manager.py
@@ -2,8 +2,6 @@ import unicornhat as uh
 from time import sleep, time
 import colorsys
 
-off = (0, 0, 0)
-
 class UnicornManager:
   def __init__(self):
     uh.set_layout(uh.PHAT)
@@ -13,18 +11,20 @@ class UnicornManager:
     for x in range(8):
       for y in range(4):
         uh.set_pixel(x, y, *color)
+    uh.show()
 
   def solid(self, color):
     self.set_light(color)
     uh.show()
 
-  def blink(self, color):
+  def off(self, _):
     off = (0, 0, 0)
     self.set_light(off)
-    uh.show()
+
+  def blink(self, color):
+    self.off(color)
     sleep(1)
     self.set_light(color)
-    uh.show()
     sleep(1)
 
   def rainbow(self, _):
@@ -40,6 +40,7 @@ class UnicornManager:
     sleep(0.005)
 
   def alert(self, color):
+    off = (0, 0, 0)
     for x in range(8):
       for y in range(4):
         pixelColor = color if ((x % 2) == 0 and (y % 2) == 0) or ((x % 2) == 1 and (y % 2) == 1) else off
